42c4f17d4adc5c5bbd95d964531772c9e00c6e34
PrashanthBabuS1998 <prashanthbabus.1rn17ec110@gmail.com>
2024-09-17 14:11:42 +0530

    Update Hello.c

--- Worknotes /Hello.c
+++ Worknotes /Hello.c
@@ -1,19 +1,1 @@
-#include "main.h"
 
-int file_Validation(char* str, FILE *fptr)
-{
-    //seek to end of file
-    fseek(fptr, 0, SEEK_END);
-
-    //if ftell is 0, which means file is empty
-    if(ftell == 0)
-    {
-        printf("File is empty.\n");
-        return FAILURE;
-    }
-    else
-    {
-        fseek(fptr, 0, SEEK_SET);
-        return SUCCESS;
-    }
-}
